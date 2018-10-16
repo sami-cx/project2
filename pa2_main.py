d8db026575bb7f245f4ef8b96465cd379c11e3cf
samantha gonzalez <sgonz114@msudenver.edu>
2018-10-15 18:35:05 -0600

    yes commit project 2

--- pa2_main.py
+++ pa2_main.py
@@ -1,3 +1,13 @@
+def float_to_int(float_list):
+    int_list = []
+    for f in float_list:
+            int_list.append(int(f))
+        return int_list
 
 if __name__ == '__main__':
-    print("hello, world!")
+    floats = [float(i) for i in range(100)]
+    print(float_to_int(floats))
+
+
+
+
